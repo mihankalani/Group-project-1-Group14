1e7ba2012e15e41c808688e6e66f1838705ece23
mihankalani <mihan.kalani@gmail.com>
2022-09-30 21:05:00 -0400

    group project 14

---
+++ src/projectjava1/task06.java
@@ -0,0 +1,20 @@
+package projectjava1;
+
+public class task06 {
+
+	public static void main(String[] args) {
+		// Write a program to swap 2 numbers without a temporary variable?
+
+				int a = 30;
+				int b = 10;
+				a = a + b;// --->a=40
+
+				b = a - b;// --->b=30
+				a = a - b;// --->a=10
+				// a=10 b=30
+
+				System.out.println("After Swapping: a=" + a);
+				System.out.println("After Swapping: b=" + b);
+	}
+
+}
---
+++ src/projectjava1/task11.java
@@ -0,0 +1,18 @@
+package projectjava1;
+
+public class task11 {
+
+	public static void main(String[] args) {
+		// Write a program to print out duplicate elements from an Array of Strings?
+
+				String[] name = { "alam", "mihan", "alam", "tara", "mihan" };
+				for (int i = 0; i < name.length; i++) {
+					for (int j = i + 1; j < name.length; j++) {
+						if (name[i].equalsIgnoreCase(name[j])) {
+							System.out.println(name[i] + " and " + name[j] + " are the same");
+						}
+					}
+				}
+	}
+
+}
